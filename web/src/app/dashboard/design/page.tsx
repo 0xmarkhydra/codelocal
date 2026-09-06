0ba0c01c5615d3ffddd7c561005cb2f5ab45c341
monkeybnb <0xmarkhydra@gmail.com>
2026-09-06 08:46:18 +0700

    feat: add CodeLocal design dashboard route

---
+++ web/src/app/dashboard/design/page.tsx
@@ -0,0 +1,15 @@
+export default function DesignPage() {
+  return (
+    <main style={{ height: "100vh", width: "100%" }}>
+      <iframe
+        title="CodeLocal Design"
+        src="https://design.codelocal.cloud"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: 0,
+        }}
+      />
+    </main>
+  );
+}
